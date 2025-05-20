2461390bc2c0c1d478bb94f6a020fd96fb06e8a6
Simon Masson <simon.masson@yahoo.fr>
2025-05-20 15:43:34 +0200

    Parameters for our ZKDilithium

--- README.md
+++ README.md
@@ -1,1 +1,60 @@
 # security-estimates
+
+## How to use:
+```
+python ZKDilithium.py
+```
+This takes few minutes to compute the security estimation (disabling the dual SIS attack reduces the computational time if you are in a hurry). It should output:
+* For Dilithium (NIST II):
+    ```
+    Dilithium
+    {'n': 256, 'k': 4, 'l': 4, 'gamma1': 131072, 'gamma2': 95232.0, 'q': 8380417, 'eta': 2, 'zeta': 350209.0, 'zeta_prime': 380929.0, 'pkdrop': 13}
+
+    === WEAK UF
+    Attack uses block-size 423 and 2304 dimensions, with 0 q-vectors
+    log2(epsilon) = -84.52, log2 nvector per run 87.78
+    shortest vector used has length l=7242509.01, q=8380417, `l<q'= 1
+    SIS & 2304 & 423 & 123 & 112 & 87
+    === STRONG UF
+    Attack uses block-size 417 and 2304 dimensions, with 0 q-vectors
+    log2(epsilon) = -82.72, log2 nvector per run 86.54
+    shortest vector used has length l=7835854.32, q=8380417, `l<q'= 1
+    SIS & 2304 & 417 & 121 & 110 & 86
+    === SECRET KEY RECOVERY
+    Primal attacks uses block-size 424 and 977 samples; dim d=2002
+    Primal & 977 & 424 & 124 & 112 & 87
+    Dual attacks uses block-size 422 and 1014 samples; dim d=2038
+    shortest vector used has length l=7347307.38, q=8380417, `l<q'= 1
+    log2(epsilon) = -43.78, log2 nvector per run 87.57
+    Dual & 1014 & 422 & 123 & 111 & 87 
+    ```
+* For our ZK-Dilithium:
+    ```
+    DilithiumZK6
+    {'n': 256, 'k': 4, 'l': 5, 'gamma1': 131072, 'gamma2': 983040.0, 'q': 2013265921, 'eta': 4, 'zeta': 2125825.0, 'zeta_prime': 3932161.0, 'pkdrop': 13}
+
+    === WEAK UF
+    Attack uses block-size 468 and 2560 dimensions, with 0 q-vectors
+    log2(epsilon) = -92.21, log2 nvector per run 97.12
+    shortest vector used has length l=47388212.06, q=2013265921, `l<q'= 1
+    SIS & 2560 & 468 & 136 & 124 & 97
+    === STRONG UF
+    Attack uses block-size 426 and 2560 dimensions, with 0 q-vectors
+    log2(epsilon) = -86.18, log2 nvector per run 88.40
+    shortest vector used has length l=85708324.96, q=2013265921, `l<q'= 1
+    SIS & 2560 & 426 & 124 & 112 & 88
+    === SECRET KEY RECOVERY
+    Primal attacks uses block-size 426 and 1017 samples; dim d=2298
+    Primal & 1017 & 426 & 124 & 112 & 88
+    Dual attacks uses block-size 424 and 1024 samples; dim d=2304
+    shortest vector used has length l=968498746.70, q=2013265921, `l<q'= 1
+    log2(epsilon) = -43.93, log2 nvector per run 87.99
+    Dual & 1024 & 424 & 124 & 112 & 87 
+    ```
+
+## Summary
+|Security| Dilithium | ZK-Dilithium |
+|-|-|-|
+| Quantum| 121  bits|124 bits|
+|Classical| 110 bits|112 bits|
+|Plausible | 86 bits|87  bits|
---
+++ ZKDilithium.py
@@ -0,0 +1,136 @@
+from MSIS_security import MSIS_summarize_attacks, MSISParameterSet
+from MLWE_security import MLWE_summarize_attacks, MLWEParameterSet
+from math import sqrt
+
+
+class UniformDilithiumParameterSet(object):
+    def __init__(self, n, k, l, gamma1, gamma2, tau, q, eta, pkdrop=0):
+        self.n = n
+        self.k = k
+        self.l = l
+        self.gamma1 = gamma1
+        self.gamma2 = gamma2
+        self.q = q
+        self.eta = eta
+        # SIS l_oo bound for unforgeability
+        self.zeta = max(gamma1, 2*gamma2 + 1 + 2**(pkdrop-1) *
+                        tau)                # Define in section 6.2
+        # SIS l_oo bound for strong unforgeability
+        # Define in section 6.2
+        self.zeta_prime = max(2*gamma1, 4*gamma2 + 1)
+        self.pkdrop = pkdrop
+
+
+# class GaussianDilithiumParameterSet(object):
+#     def __init__(self, n, k, l, sigma, q, eta, pkdrop=0):
+#         self.n = n
+#         self.k = k
+#         self.l = l
+#         self.sigma = sigma
+#         self.q = q
+#         self.eta = eta
+#         self.pkdrop = pkdrop
+#         self.B = 2*equation5(self)
+
+
+# def equation5(dps):
+#     B2 = ((1.05 * dps.sigma * sqrt((dps.k + dps.l)*dps.n))**2
+#           +(2**(dps.pkdrop-1) * sqrt(60*dps.n*dps.k))**2)
+#     return sqrt(B2)
+
+n = 256
+q_dilithium = 8380417
+q_DilithiumZK = 2013265921  # babybear
+
+Dilithium = UniformDilithiumParameterSet(
+    n, 4, 4, 2**17, (q_dilithium-1)/88, 39, q_dilithium, 2, pkdrop=13)
+DilithiumZK1 = UniformDilithiumParameterSet(
+    n, 4, 4, 2**17, (q_dilithium-1)/88, 39, q_DilithiumZK, 2, pkdrop=13)
+DilithiumZK2 = UniformDilithiumParameterSet(
+    n, 4, 4, 2**17, (q_dilithium-1)/88, 39, q_DilithiumZK, 10, pkdrop=13)
+DilithiumZK3 = UniformDilithiumParameterSet(
+    n, 4, 5, 2**17, (q_dilithium-1)/88, 39, q_DilithiumZK, 2, pkdrop=13)
+DilithiumZK4 = UniformDilithiumParameterSet(
+    n, 4, 5, 2**17, (q_dilithium-1)/88, 39, q_DilithiumZK, 4, pkdrop=13)
+DilithiumZK5 = UniformDilithiumParameterSet(
+    n, 4, 5, 2**17, (q_DilithiumZK-1)/2**10, 39, q_DilithiumZK, 4, pkdrop=13)
+DilithiumZK6 = UniformDilithiumParameterSet(
+    n, 4, 5, 2**17, (q_DilithiumZK-1)/2**11, 39, q_DilithiumZK, 4, pkdrop=13)
+
+
+all_params_unif = [
+    ("Dilithium", Dilithium),
+    # ("DilithiumZK1", DilithiumZK1),
+    # ("DilithiumZK2", DilithiumZK2),
+    # ("DilithiumZK3", DilithiumZK3),
+    # ("DilithiumZK4", DilithiumZK4),
+    # ("DilithiumZK5", DilithiumZK5),
+    ("DilithiumZK6", DilithiumZK6),
+]
+
+all_params = all_params_unif
+
+
+def Dilithium_to_MSIS(dps, strong_uf=False):
+    if strong_uf:
+        return MSISParameterSet(dps.n, dps.k + dps.l + 1, dps.k, dps.zeta_prime, dps.q, norm="linf")
+    else:
+        return MSISParameterSet(dps.n, dps.k + dps.l + 1, dps.k, dps.zeta, dps.q, norm="linf")
+
+
+def Dilithium_to_MLWE(dps):
+    return MLWEParameterSet(dps.n, dps.l, dps.k, dps.eta, dps.q, distr="uniform")
+
+
+text_SIS = ["BKZ block-size $b$ to break SIS", "Best Known Classical bit-cost",
+            "Best Known Quantum bit-cost", "Best Plausible bit-cost"]
+text_LWE = ["BKZ block-size $b$ to break LWE", "Best Known Classical bit-cost",
+            "Best Known Quantum bit-cost", "Best Plausible bit-cost"]
+
+
+table_SIS = [4*[0] for i in range(4)]
+table_LWE = [4*[0] for i in range(4)]
+j = 0
+
+for (scheme, param) in all_params_unif:
+    print("\n"+scheme)
+    print(param.__dict__)
+    print("")
+    print("=== WEAK UF")
+    v = MSIS_summarize_attacks(Dilithium_to_MSIS(param))
+    print("=== STRONG UF")
+    v = MSIS_summarize_attacks(Dilithium_to_MSIS(param, strong_uf=True))
+    for i in range(4):
+        table_SIS[i][j] = v[i]
+    print("=== SECRET KEY RECOVERY")
+    v = MLWE_summarize_attacks(Dilithium_to_MLWE(param))
+    for i in range(4):
+        table_LWE[i][j] = v[i]
+    j += 1
+
+print("UNIFORM DILITHIUM TABLE")
+print("========================")
+
+print("\\hline")
+for j in range(4):
+    print(text_SIS[j]+" & "),
+    for i in range(4):
+        print(table_SIS[j][i]),
+        if i < 3:
+            print(" & "),
+    print("\\\\")
+print("\\hline")
+for j in range(4):
+    print(text_LWE[j]+" & "),
+    for i in range(4):
+        print(table_LWE[j][i]),
+        if i < 3:
+            print(" & "),
+    print("\\\\")
+print("\\hline")
+
+print("========================")
+
+table_SIS = [4*[0] for i in range(4)]
+table_LWE = [4*[0] for i in range(4)]
+j = 0
